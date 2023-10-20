32a8f4a707ea866c38c6844a5fdcb95c1564fdb0
student <student@gnu.ac.in>
2023-10-20 09:01:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_10_21012021011/Contect.kt
@@ -0,0 +1,9 @@
+package com.example.mad_practical_10_21012021011
+
+import java.io.Serializable
+
+class Contect(val id:String, val name: String,val emailid: String,val phoneNo: String , val address: String , val latitude: Double , val longitude: Double):Serializable{
+
+
+
+}
