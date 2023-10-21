c238974fa986c9c2cb24af2a5b9c7ee2ff41d818
student <student@gnu.ac.in>
2023-10-21 11:12:40 +0530

    p

--- app/src/main/java/com/example/mad_practical_10_21012021011/ContectAdepter.kt
+++ app/src/main/java/com/example/mad_practical_10_21012021011/ContectAdepter.kt
@@ -5,12 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 
-class ContectAdepter (val context : Context , val array : ArrayList<Contect>) : ArrayAdapter<Contect>(context, 0 , array) {
+class ContectAdepter (context : Context , val array : ArrayList<Contect>) : ArrayAdapter<Contect>(context, 0 , array) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.listviewlayout,parent)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.listviewlayout,parent , false)
+
+        view.findViewById<TextView>(R.id.textView2).text = array.get(position).name
+        view.findViewById<TextView>(R.id.textView3).text = array.get(position).phoneNo
+        view.findViewById<TextView>(R.id.textView4).text = array.get(position).address
+
+
+
         return view
     }
+
 
 }
